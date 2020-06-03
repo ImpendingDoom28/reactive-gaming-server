ae3f14c4df1832cf7aac97811a04cecceb047415
Serejenka <mikheevs11@gmail.com>
2020-06-03 18:03:11 +0300

    Siggil Init

---
+++ src/main/java/ru/sergey/siggil/SiggilApplication.java
@@ -0,0 +1,13 @@
+package ru.sergey.siggil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiggilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiggilApplication.class, args);
+    }
+
+}
